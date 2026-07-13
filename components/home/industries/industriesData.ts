09e6a4f7228e811fc5fd75ed7596af1734b345b7
Vinodgowda G <vinodgowdag351@gmail.com>
2026-07-13 11:31:30 +0530

    Update industries data

--- components/home/industries/industriesData.ts
+++ components/home/industries/industriesData.ts
@@ -1,26 +1,26 @@
 export const industries = [
   {
     title: "Industrial Warehouses",
-    image: "/images/industries/warehouse.jpg",
+    image: "/images/industries/warehouse.png",
   },
   {
     title: "Manufacturing Plants",
-    image: "/images/industries/manufacturing.jpg",
+    image: "/images/industries/manufacturing.png",
   },
   {
     title: "Commercial Buildings",  
-    image: "/images/industries/commercial.jpg",
+    image: "/images/industries/commercial.png",
   },
   {
     title: "Logistics & Distribution",
-    image: "/images/industries/logistics.jpg",
+    image: "/images/industries/logistics.png",
   },
   {
     title: "Agricultural Buildings",
-    image: "/images/industries/agriculture.jpg",
+    image: "/images/industries/agriculture.png",
   },
   {
     title: "Infrastructure Projects",
-    image: "/images/industries/infrastructure.jpg",
+    image: "/images/industries/infrastructure.png",
   },
 ];
